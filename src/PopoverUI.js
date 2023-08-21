e6993685d386463d463b411e38888abc9cb73cfd
novemberizing <novemberizing@gmail.com>
2023-08-21 13:34:42 +0900

    apply on window.scrollY

--- src/PopoverUI.js
+++ src/PopoverUI.js
@@ -10,8 +10,11 @@ export default class PopoverUI {
         }
         const button = novemberizing.dom.parent(event.target, "button");
         const rect = button.getBoundingClientRect();
+        target.addEventListener("click", e => {
+            PopoverUI.toggle(event, id);
+        }, { once: true });
         target.style.left = `${rect.left}px`;
-        target.style.top = `${rect.top - rect.height - 16}px`;
+        target.style.top = `${rect.top + window.scrollY - rect.height - 16}px`;
     }
     static toggle(event, id) {
         const target = document.getElementById(id);
