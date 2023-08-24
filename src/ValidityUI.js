a5e0af31260aeaedf9ff3d8e395c8197492266eb
novemberizing <novemberizing@gmail.com>
2023-08-24 15:52:24 +0900

    element.value not working modify setAttribute("value",...)

--- src/ValidityUI.js
+++ src/ValidityUI.js
@@ -3,14 +3,16 @@ export default class ValidityUI {
     static check(element, condition = true) {
         if(element.validity) {
             if(!element.validity.valid || !condition) {
+                if(condition === false) {
+                    element.setAttribute(value, "");
+                }
+
                 if(element.classList.contains("is-valid")) {
                     element.classList.replace("is-valid", "is-invalid");
                 } else {
                     element.classList.add('is-invalid');
                 }
-                if(condition === false) {
-                    element.value = "";
-                }
+
                 element.focus();
     
                 return false;
