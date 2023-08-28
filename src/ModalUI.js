acdb3cf128f2030bba8f4182a34def8c0fbf3eb8
novemberizing <novemberizing@gmail.com>
2023-08-28 17:12:59 +0900

    update

--- src/ModalUI.js
+++ src/ModalUI.js
@@ -1,35 +1,54 @@
 
 export default class ModalUI {
     static #modal = null;
+    static #submit = null;
+    static #cancel = null;
 
-    static #show(modal) {
+    static submit() {
+        if(ModalUI.#modal) {
+            const submit = ModalUI.#submit;
+            ModalUI.#modal._element.addEventListener("hidden.bs.modal", e => {
+                console.log(submit);
+                if(submit) submit(e);
+            }, { once: true });
+            ModalUI.#modal.hide();
+        }
+    }
+
+    static cancel() {
+        ModalUI.#modal.hide();
+    }
+
+    static hide() {
+        ModalUI.#modal.hide();
+    }
+
+    static #show(modal, submit, cancel) {
         ModalUI.#modal = modal;
+        ModalUI.#submit = submit;
+        ModalUI.#cancel = cancel;
+
         ModalUI.#modal._element.addEventListener("hidden.bs.modal", e => {
             ModalUI.#modal = null;
+            ModalUI.#submit = null;
+            ModalUI.#cancel = null;
         }, { once: true });
+
         ModalUI.#modal.show();
     }
 
-    static hide(callback) {
+    static show(modal, submit, cancel) {
         if(ModalUI.#modal) {
-            if(typeof callback === "function") {
-                ModalUI.#modal._element.addEventListener("hidden.bs.modal", e => {
-                    ModalUI.#modal = null;
-                    callback();
-                }, { once: true });
-            }
-            ModalUI.#modal.hide();
-        }
-    }
-
-    static show(modal) {
-        if(ModalUI.#modal) {
-            ModalUI.#modal.hide();
             ModalUI.#modal._element.addEventListener("hidden.bs.modal", e => {
-                ModalUI.#show(modal);
+                if(cancel) cancel(e);
+
+                ModalUI.#show(modal, submit, cancel);
             }, { once: true });
-        } else {
-            ModalUI.#show(modal);
+
+            ModalUI.#modal.hide();
+            return;
         }
+
+        ModalUI.#show(modal, submit, cancel);
     }
 }
