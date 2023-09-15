3874702a0a2b0620090883ee69aaf355b3c182ad
novemberizing <novemberizing@gmail.com>
2023-09-15 10:51:54 +0900

    Add ValidityUI's valid & invalid method.

--- src/ValidityUI.js
+++ src/ValidityUI.js
@@ -1,5 +1,19 @@
 
 export default class ValidityUI {
+    static valid(element) {
+        if(element.classList.contains("is-invalid")) {
+            element.classList.replace("is-invalid", "is-valid");
+        } else {
+            element.classList.add('is-valid');
+        }
+    }
+    static invalid(element) {
+        if(element.classList.contains("is-valid")) {
+            element.classList.replace("is-valid", "is-invalid");
+        } else {
+            element.classList.add('is-invalid');
+        }
+    }
     static check(element, condition = true) {
         if(element.validity) {
             if(!element.validity.valid || !condition) {
